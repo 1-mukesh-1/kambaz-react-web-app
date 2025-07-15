22f02055434fcd2073fb7afafccbdc9a9272f77b
mukesh cheemakurthi <51417684+1-mukesh-1@users.noreply.github.com>
2025-07-15 01:38:47 -0400

    labs done

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,19 +1,29 @@
+import { Container } from "react-bootstrap";
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
 import Corners from "./Corners";
 import Dimensions from "./Dimensions";
+import Flex from "./Flex";
 import Float from "./Float";
 import ForegroundColors from "./ForegroundColors";
+import GridLayout from "./GridLayout";
 import "./index.css";
 import Margins from "./Margins";
 import Padding from "./Padding";
 import Positions from "./Positions";
+import ReactIcons from "./ReactIcons";
 import Zindex from "./Zindex";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>
@@ -86,5 +96,23 @@ export default function Lab2() {
       <Zindex />
 
       <Float />
-    </div>);
+
+      <GridLayout />
+
+      <Flex />
+
+      <ReactIcons />
+
+      <BootstrapGrids />
+
+      <ScreenSizeLabel />
+
+      <BootstrapTables />
+
+      <BootstrapLists />
+
+      <BootstrapForms />
+
+      <BootstrapNavigation />
+    </ Container>);
 }
