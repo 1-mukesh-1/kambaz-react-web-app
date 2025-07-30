8c3bdd348d1d2115cc372f4e6ea25d2c23acd6c6
mukesh cheemakurthi <51417684+1-mukesh-1@users.noreply.github.com>
2025-07-30 16:57:25 -0400

    lab 4

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,23 +1,65 @@
-import ReduxExamples from "./ReduxExamples";
-// import ClickEvent from "./ClickEvent";
-// import PassingDataOnEvent from "./PassingDataOnEvent";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 export default function Lab4() {
-    function sayHello() {
-        alert("Hello");
-    }
-    return (
-        <div id="wd-passing-functions">
-            <h2>Lab 4</h2>
-            ...
-            <PassingFunctions theFunction={sayHello} />
+  function sayHello() {
+    alert("Hello");
+  }
 
-            <ReduxExamples />
+  return (
+    <div className="container" id="wd-lab4">
+      <h2>Lab 4</h2>
 
-            {/* <PassingFunctions theFunction={ClickEvent} />
-            <PassingFunctions theFunction={PassingDataOnEvent} /> */}
-        </div>
-    );
+      <h3>Component State and Event Handling</h3>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+
+      <h3>useState Examples</h3>
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+
+      <ReduxExamples />
+    </div>
+  );
 }
+
+// import ReduxExamples from "./ReduxExamples";
+// // import ClickEvent from "./ClickEvent";
+// // import PassingDataOnEvent from "./PassingDataOnEvent";
+// import PassingFunctions from "./PassingFunctions";
+
+// export default function Lab4() {
+//     function sayHello() {
+//         alert("Hello");
+//     }
+//     return (
+//         <div id="wd-passing-functions">
+//             <h2>Lab 4</h2>
+//             ...
+//             <PassingFunctions theFunction={sayHello} />
+
+//             <ReduxExamples />
+
+//             {/* <PassingFunctions theFunction={ClickEvent} />
+//             <PassingFunctions theFunction={PassingDataOnEvent} /> */}
+//         </div>
+//     );
+// }
 
