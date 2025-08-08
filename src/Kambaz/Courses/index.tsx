09020e4290bddf544f87e8bb658c4f4c5ab031af
mukesh cheemakurthi <51417684+1-mukesh-1@users.noreply.github.com>
2025-08-08 03:18:21 -0400

    assignment fix

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -5,17 +5,18 @@ import { FaAlignJustify } from "react-icons/fa6";
 import { Route, Routes, useParams, useLocation } from "react-router";
 import CourseNavigation from "./Navigation";
 import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
 
 export default function Courses({ courses }: { courses: any[] }) {
   const { cid } = useParams();
-    const course = courses.find((course: any) => course._id === cid);
-    const { pathname } = useLocation();
+  const course = courses.find((course: any) => course._id === cid);
+  const { pathname } = useLocation();
 
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-                {course && course.name + " > " + pathname.split("/")[4]}
+        {course && course.name + " > " + pathname.split("/")[4]}
       </h2>
 
       <div className="d-flex">
@@ -26,11 +27,12 @@ export default function Courses({ courses }: { courses: any[] }) {
           <Routes>
             <Route path="Home" element={<Home />} />
             <Route path="Modules" element={<Modules />} />
-                        <Route path="Piazza" element={<h2>Piazza</h2>} />
-                        <Route path="Zoom" element={<h2>Zoom</h2>} />
+            <Route path="Piazza" element={<h2>Piazza</h2>} />
+            <Route path="Zoom" element={<h2>Zoom</h2>} />
             <Route path="Assignments" element={<Assignments />} />
-                        <Route path="Quizzes" element={<h2>Quizzes</h2>} />
-                        <Route path="Grades" element={<h2>Grades</h2>} />
+            <Route path="Assignments/:aid/*" element={<AssignmentEditor />} />
+            <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+            <Route path="Grades" element={<h2>Grades</h2>} />
             <Route path="People" element={<PeopleTable />} />
           </Routes>
         </div>
